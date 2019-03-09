d54fcdb244be713315c487cae9b95ce160e2f3ce
brian <bdcase@gmail.com>
2019-03-09 09:16:53 -0600

    added bootstrap files, also began work on index page

---
+++ site/index.php
@@ -0,0 +1,33 @@
+<html lang="en"> 
+<head>
+  
+    <!-- Bootstrap - Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Test</title>
+</head>
+
+<body>
+    <p>&nbsp;</p>
+    <div class="container">
+
+    <!-- table -->
+    <table class='table' data-toggle="table" data-url="student_info.txt">
+        <thead>
+            <TR>
+                <TH data-field="Student Name" data-align="center" data-sortable="true">Student Name</TH>
+                <TH data-field="GPA" data-align="center" data-sortable="true">GPA</TH>
+                <TH data-field="Classes" data-align="center" data-sortable="true">Classes</TH>
+            </TR>
+        </thead>
+    </table>
+
+    </div>
+<!-- Latest compiled and minified JavaScript -->
+<script src="public/js/jquery-1.11.1.min.js"></script>
+<script src="public/js/bootstrap.min.js"></script>
+</body>
+
+</html>
