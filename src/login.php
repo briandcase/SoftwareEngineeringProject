83591a8bc77809f275c9b53df9e5b3ef5e081dbe
jdj005 <jeremy_djohnson@yahoo.com>
2019-04-21 22:29:03 -0500

    Update login.php

--- src/login.php
+++ src/login.php
@@ -7,8 +7,8 @@
     <title>Login</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
     <!-- Bootstrap core CSS -->
-    <link href="../public/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../public/css/signin.css" rel="stylesheet">
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/signin.css">
 
     <style>
       .bd-placeholder-img {
