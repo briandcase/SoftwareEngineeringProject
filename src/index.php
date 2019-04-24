36eb997b9d8c1e4f0e5060561e6014e1b52ef1d2
guy-176251 <longnguyen176251@gmail.com>
2019-04-23 19:59:35 -0500

    Update index.php
    changed the wrong index file

--- src/index.php
+++ src/index.php
@@ -29,41 +29,105 @@ if (!$_SESSION['UserData']['admin']) {
 }
 
 ?>
-
 <div class="container">
   <div class="row">
   <div class="col-2"></div>
   <div class="col-8">
+<h1>Database Manager</h1>
 <a href="add.php">Add</a>
 <br>
 <table class="table" border="1">
     <thead>
+        <tr>
+            <th scope="col" colspan="4"> </th>
+            <th scope="col" colspan="4">Courses</th>
+            <th scope="col" colspan="3">Course 1 Exam Scores</th>
+            <th scope="col" colspan="3">Course 2 Exam Scores</th>
+            <th scope="col" colspan="3">Course 3 Exam Scores</th>
+            <th scope="col" colspan="3">Course 4 Exam Scores</th>
+        </tr>
       <tr>
         <th scope="col">ID</th>
-        <th scope="col">Firstname</th>
-        <th scope="col">Lastname</th>
-        <th scope="col">examScore</th>
-        <th scope="col">courses</th>
-        <th scope="col">gpa</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">GPA</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
+        <th scope="col">4</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
       </tr>
     </thead>
     <tbody>
         <?php
             //fetch data from json
-            $data = file_get_contents('members.json');
-            //decode into php array
-            $data = json_decode($data);
+            $data = json_decode(file_get_contents('members.json'), true);
+
+            if($data === null) echo 'Array is null.';
 
             $index = 0;
             foreach($data as $row){
                 echo "
                     <tr>
-                        <td>".$row->id."</td>
-                        <td>".$row->firstname."</td>
-                        <td>".$row->lastname."</td>
-                        <td>".$row->examScore."</td>
-                        <td>".$row->courses."</td>
-                        <td>".$row->gpa."</td>
+                        <td>".$row['id']."</td>
+                        <td>".$row['firstname']."</td>
+                        <td>".$row['lastname']."</td>";
+                        #"<td>".$row['gpa']."</td>";
+
+                $gpa_calc = [];
+                foreach($row['courses'] as $course){
+                    if(count($course['exams']) > 0){
+                        $avg = array_sum($course['exams']) / count($course['exams']);
+                        $avg = $avg * 4.0 / 100;
+                        array_push($gpa_calc, $avg);
+                    }
+                }
+
+                if(count($gpa_calc) == 0){
+                    echo "<td><b>N/A</b></td>";
+                } else {
+                    $avg = array_sum($gpa_calc) / count($gpa_calc);
+                    echo "<td>".round($avg, 1)."</td>";
+                }
+
+                $course_num = 0;
+                foreach($row['courses'] as $course){
+                    echo "<td>".$course['name']."</td>";
+                    $course_num++;
+                }
+
+                if($course_num < 4){
+                    for($i = 0; $i < 4 - $course_num; $i++) echo "<td></td>";
+                }
+
+                foreach($row['courses'] as $course){
+                    $exam_num = 0;
+                    foreach($course['exams'] as $score){
+                            echo "<td>".$score."</td>";
+                            $exam_num++;
+                    }
+
+                    if($exam_num < 3){
+                        for($i = 0; $i < 3 - $exam_num; $i++) echo "<td></td>";
+                    }
+                }
+
+                if($course_num < 4){
+                    for($i = 0; $i < 4 - $course_num; $i++) echo "<td></td><td></td><td></td>";
+                }
+
+                echo "
                         <td><a href='edit.php?index=".$index."'>Edit</a></td>
                         <td><a href='delete.php?index=".$index."'>Delete</a></td>
                     </tr>
