d7244fb3d51cd41fd79c376a0f097fa0e1b9cf00
guy-176251 <longnguyen176251@gmail.com>
2019-04-23 20:24:19 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <head>
 
 <!-- Bootstrap - Latest compiled and minified CSS -->
-<link rel="stylesheet" href="public/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap.min.css">
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
